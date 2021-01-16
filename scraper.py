2cc6b11ad365ec6a6764520e44004d7a516e07ec
Aymeric R <aymeric.roucher@gmail.com>
2021-01-16 16:41:12 +0100

    Add multiprocessing.

--- scraper.py
+++ scraper.py
@@ -1,22 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import concurrent.futures
+import threading
+import time
+from tqdm import tqdm
 
-# Our base URL includes the term 'france', which precises we want the french prices for the wines
-BASE_URL = "https://www.idealwine.com/uk/wine-prices/{}-1er-grand-cru-classe.jsp"
-
-session = requests.Session()
 HEADERS = {
     "user-agent": (
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 "
         "(KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36"
     )
 }
-DATA_DIR = "data"
-FILENAME = "idealwine-data"
+BASE_URL = 'https://www.idealwine.com'
+SEARCH_URL = BASE_URL+"/uk/wine-prices/{}.jsp"
 
 # Let us first create a list of all grands crus from the 1855 ranking of Medoc
-premiers_grands_crus_classes_rouges = [
+premiers_grands_crus_classes = [
     'Château Lafite Rothschild Pauillac',
     'Château Latour Pauillac',
     'Château Mouton Rothschild Pauillac',
@@ -24,9 +24,9 @@ premiers_grands_crus_classes_rouges = [
     'Château Haut-Brion Pessac-léognan'
     ]
 for i in range(len(premiers_grands_crus_classes)):
-    premiers_grands_crus_classes[i] = premiers_grands_crus_classes[i].replace(' ', '-')+'-1er-cru'
+    premiers_grands_crus_classes[i] = premiers_grands_crus_classes[i].replace(' ', '-')+'-1er-grand-cru-classe'
 
-deuxiemes_grands_crus_classes_rouges = [
+deuxiemes_grands_crus_classes = [
     'Château Brane-Cantenac Margaux',
     'Château Durfort-Vivens Margaux',
     'Château Lascombes Margaux',
@@ -43,7 +43,7 @@ deuxiemes_grands_crus_classes_rouges = [
     'Château Léoville Poyferré Saint-Julien'
     ]
 for i in range(len(deuxiemes_grands_crus_classes)):
-    deuxiemes_grands_crus_classes[i] = deuxiemes_grands_crus_classes[i].replace(' ', '-')+'-2eme-cru'
+    deuxiemes_grands_crus_classes[i] = deuxiemes_grands_crus_classes[i].replace(' ', '-')+'-2eme-grand-cru-classe'
 
 class Scraper:
     """Scraper for iDealwine.com to collect wine reviews. Adapted from @zackthoutt webscraper."""
@@ -51,22 +51,24 @@ class Scraper:
     def __init__(
         self, vineyard_list, min_vintage
     ):
+        self.start_time = time.time()
         self.session = requests.Session()
         self.vineyard_list = vineyard_list
         self.min_vintage = min_vintage
         self.result = pd.DataFrame(0, index=range(2020,min_vintage-1, -1), columns=vineyard_list)
+        self.tqdm_bar=None
 
     def scrape_site(self):
-        for vineyard in self.vineyard_list:
-            print('Target vineyard: ', vineyard)
-            self.scrape_vineyard(vineyard)
-        print("Scrape finished...")
+        print('Beginning to scrape iDealwine...')
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            self.tqdm_bar = tqdm(executor.map(self.scrape_vineyard, self.vineyard_list), total=len(self.vineyard_list))
+            res = list(self.tqdm_bar)
+        print("Scrape finished.")
         return self.result
 
     def scrape_vineyard(self, vineyard, retry_count=0):
-        page_url = BASE_URL.format(vineyard)
+        page_url = SEARCH_URL.format(vineyard)
         try:
-            print(page_url)
             response = self.session.get(page_url, headers=HEADERS)
 
         except:
@@ -80,22 +82,18 @@ class Scraper:
         soup = BeautifulSoup(response.content, "html.parser")
         # With the precision of our request, the target wine normally shows up first, thus we get it with index 0
         wine_link = soup.find_all("table", {"id": "tbResult"})[0].a['href']
-        
-        wine_url = 'https://www.idealwine.com' + wine_link
-        print(wine_url)
+        wine_url = BASE_URL + wine_link
         
         # Initialize vintage by the highest, i.e. most recent vintage referenced on the website
         wine_response = self.session.get(wine_url, headers=HEADERS)
         wine_soup = BeautifulSoup(wine_response.content, "html.parser")
         max_vintage_str = wine_soup.find_all('a', {'class':'ola selected-vintage'})[0].text
-        vintage = int(max_vintage_str)
+        max_vintage = int(max_vintage_str)
 
-        while vintage >= self.min_vintage:
-            print(max_vintage_str, vintage, wine_url.replace(max_vintage_str, str(vintage)))
+        for vintage in range(max_vintage, self.min_vintage-1, -1):
             wine_response = self.session.get(wine_url.replace(max_vintage_str, str(vintage)), headers=HEADERS)
             wine_soup = BeautifulSoup(wine_response.content, "html.parser")
             self.result.loc[vintage, vineyard]=self.parse_vineyard(wine_soup)
-            vintage -= 1
         return
 
     def parse_vineyard(self, wine_soup):
@@ -117,6 +115,6 @@ if __name__ == "__main__":
         2010
     )
     table = scraper.scrape_site()
-    print(table.head(20).to_string())
-    table.to_excel('table.xls', encoding='utf-16')
+    print(table.iloc[:5,:5].to_string())
+    table.to_excel('table.xlsx', encoding='utf-16')
 
